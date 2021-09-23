073149f28e81e933bcefedb2926879e80b9926bc
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-09-23 02:39:06 -0500

    Agregando controlador person y sus funciones

---
+++ app/Http/Controllers/API/PersonController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Person;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    public function getAll()
+    {
+        $data = Person::get();
+        return response()->json($data, 200);
+    }
+
+    public function create(Request $request)
+    {
+        $data['name'] = $request['name'];
+        $data['email'] = $request['email'];
+        $data['phone'] = $request['phone'];
+        Person::create($data);
+        return response()->json([
+            'message' => "Successfully created",
+            'success' => true
+        ], 200);
+    }
+
+    public function delete($id)
+    {
+        $res = Person::find($id)->delete();
+        return response()->json([
+            'message' => "Successfully deleted",
+            'success' => true
+        ], 200);
+    }
+
+    public function get($id)
+    {
+        $data = Person::find($id);
+        return response()->json($data, 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data['name'] = $request['name'];
+        $data['email'] = $request['email'];
+        $data['phone'] = $request['phone'];
+        Person::find($id)->update($data);
+        return response()->json([
+            'message' => "Successfully updated",
+            'success' => true
+        ], 200);
+    }
+}
